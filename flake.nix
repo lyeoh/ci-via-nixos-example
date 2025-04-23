c979fff9d43d46bbf0368d0332d13871ba3e86d3
Luke Yeo <luke.yeo@blakeclough.com>
2025-04-23 23:21:10 +0100

    add files

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Example Python project tested with Nix flakes";
+
+  inputs = {
+    nixpkgs.url       = "github:NixOS/nixpkgs/nixos-24.05";
+    flake-utils.url   = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        python = pkgs.python311;           # pin the interpreter once
+      in
+      {
+        ### Dev shell ────────────────
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            python
+            python.pkgs.pip
+            python.pkgs.setuptools
+            python.pkgs.pytest
+            python.pkgs.black
+          ];
+        };
+
+        ### CI checks ────────────────
+        # `nix flake check` will run this automatically.
+        checks.pytests = pkgs.runCommand "pytest" { } ''
+          export HOME=$TMPDIR        # pytest likes a writable HOME
+          ${python}/bin/python -m pytest -q ${self}
+          touch $out                 # produce a dummy artifact
+        '';
+      });
+}
